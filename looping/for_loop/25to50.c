23d7d4c99709f4cb76702177a615277b1b26e6ab
Rajan17104 <rajanhingarajiya17104@gmail.com>
2023-03-24 22:45:30 +0530

    24-3

---
+++ looping/for_loop/25to50.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+
+	int i;
+
+	for (i=25; i<=50; i=i+5) {
+		printf("%d\n",i);
+	}
+
+	return 0;
+}
---
+++ looping/for_loop/sum1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() {
+	
+	int i,n,a,b,c,	avg;
+	
+	printf("enter any number:");
+	scanf("%d %d %d",&a,&b,&c);
+	
+	for(i=1; i<=avg; i++){
+		
+		avg = a+b+c/3;
+	}
+	
+	printf("avrage is %d",avg);
+	
+	return 0;
+}
+
