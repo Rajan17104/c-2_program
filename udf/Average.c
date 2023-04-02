142e4a3459bdfd9731fb5b70825c5ca6b9576ea9
Rajan17104 <rajanhingarajiya17104@gmail.com>
2023-04-02 06:32:53 +0530

    1-4-homework

---
+++ udf/Average.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+float Average(float n) {
+	
+	float i, avg,sum=0,no;
+	
+	for (i=1; i<=n; i++) {
+		printf("Please enter any Value:");
+		scanf("%f", &no);
+		
+        sum = sum + no;
+        avg = sum / 4;
+	}
+	
+	printf("your avg is:%.2f",avg);
+}
+
+int main() {
+	
+	float n;
+	
+	printf("enter any number:");
+	scanf("%.2f",&n);
+	
+	Average(n);
+	
+	return 0;
+}
