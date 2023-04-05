eb0c292f3d1f62d8ffa0d2d11d4fff15f782b268
swatidetroja <guddurani.294@gmail.com>
2023-04-05 08:57:02 +0530

    5-4-string

---
+++ string/repeat-remove.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+
+	int i,j,k;
+	char str[100];
+	
+	printf("please enter a string:");
+	gets(str);
+	
+	for(i=0;str[i] != '\0';i++){
+		for(j=i+1; str[j] != '\0';j++){
+			if(str[i] == str[j]){
+				for(k=j; str[k] != '\0';k++){
+				str[k]=str[k+1];
+				}
+			}
+		}
+	}
+		
+	puts(str);
+		
+	
+
+	return 0;
+}
---
+++ string/string-Example.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+
+	int n;
+
+	printf("please enter a string:");
+	gets(str);
+	
+	n =strlen(str);
+	
+	printf("%d",n);
+
+
+	return 0;
+}
