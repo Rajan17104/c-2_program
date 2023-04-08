11f84e829c88fbc21d6388fa078cb07f5ae5ae14
swatidetroja <guddurani.294@gmail.com>
2023-04-08 08:53:33 +0530

    8-4-pattern

---
+++ pattern/6.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(){
+
+	int i,j;
+	
+	for(i=5; i>=1; i--){
+		for(j=i; j<=5; j++){
+			printf("%d",j);
+		}
+		printf("\n");
+	}
+
+
+
+	return 0;
+}
+
+/*Output:-
+
+5
+45
+345
+2345
+12345
