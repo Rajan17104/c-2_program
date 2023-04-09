12bd2341b4fad2f555a77eb718b22827dbdd0abf
swatidetroja <guddurani.294@gmail.com>
2023-04-09 08:59:21 +0530

    9-4-pattern

---
+++ pattern/14.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+
+	int i,j,k;
+	
+	for(i=1 ;i<=5; i++){
+		for(k=5; k>i; k--){
+			printf(" ");
+		}
+		for(j=1; j<=i; j++){
+			printf("%d",j);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
+
+/*output:-
+
+    1
+   12
+  123  
+ 1234
+12345  
